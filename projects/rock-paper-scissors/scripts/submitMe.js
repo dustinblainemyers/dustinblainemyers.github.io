9448e83acbcf0c8e88e655fb335b2df04d1e3d36
dustinblainemyers <45862970+dustinblainemyers@users.noreply.github.com>
2019-12-17 21:40:37 -0500

    auto save

--- projects/rock-paper-scissors/scripts/submitMe.js
+++ projects/rock-paper-scissors/scripts/submitMe.js
@@ -2,9 +2,7 @@
 function submitMe(playerSelection) {
     document.getElementById("roundresults").innerHTML = ' ';
     
-    /*document.getElementById("currentRound").innerHTML = gameObject.roundCurrent;
-    //need to change gameTally to roundTally or something more appropriate so everything
-    //matches. */
+    
 
     gameObject.updateScoreboard();
 
